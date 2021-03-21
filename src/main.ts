8a65ae16a0168afea7bd623111add4a713ff7b1b
Tyler Wright <tylerxwright@gmail.com>
2021-03-21 12:46:24 -0400

    Trying to figure out how to output

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import * as core from '@actions/core'
 async function run(): Promise<void> {
     try {
         core.setOutput("Hello", "World");
+        core.info("Hello")
     } catch (error) {
         core.setFailed(error.message);
     }
